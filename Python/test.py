6231fb8798c567ea91d743a458373516f69df438
Swapnil Bandwal <swapllbandwal@gmail.com>
2023-03-01 01:39:10 +0400

    basic python

---
+++ Python/test.py
@@ -0,0 +1,28 @@
+message = "Hello Swapnil"
+print(message)
+
+price = 19.25
+first_Name = "Swapnil"
+is_online = False 
+age = 20 
+print(age)
+
+#Taking Inputs and converting String to Int
+name = input("What is your name ? ")
+print("Hello Mr. "+name)
+
+birth_year = input("Enter your birth year")
+
+age = 2022 - int(birth_year)
+print(age)
+
+#simple calculator program
+first = input("First: ")
+second = input("Second: ")
+sum = int(first) + int(second)
+print("Sum is : "+str(sum)) # we can only concatinate Same Data types 
+
+#String operations
+course = "Python for Biginers"
+print(course.upper())
+print(course.find("P"))
